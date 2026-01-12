4692da4b5e2e1a05758a065a4468fada1e627c9b
jeonnni <ymymda0711@gmail.com>
2026-01-12 23:21:41 +0900

    async-await

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState("");
 
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.bz/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
   useEffect(()=>{
-    fetch(`https://yts.bz/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
-      .then(response => response.json())
-      .then(json => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      })
-      .catch((error)=>{
-        console.log(error);
-        setLoading(false);
-      })
+    getMovies()
   },[])
+  
   console.log(movies);
   
   return (
